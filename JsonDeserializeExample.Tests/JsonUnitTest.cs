04574db39b6bd80e071aca55fe6343f70627b845
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2019-11-18 18:56:25 -0300

    deserialize json into Person object reading file content.

--- JsonDeserializeExample.Tests/JsonUnitTest.cs
+++ JsonDeserializeExample.Tests/JsonUnitTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Text.Json;
 using JsonDeserializeExample.Library.Entity;
 using Xunit;
@@ -18,6 +20,30 @@ namespace JsonDeserializeExample.Tests
             Assert.Equal(18, person.Age);
             Assert.Equal("Address Example", person.Address);
             Assert.Equal("02028-525", person.ZipCode);
+        }
+
+        [Fact]
+        public void ShouldDeserializeFileContentIntoPerson()
+        {
+            var codeBaseUrl = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
+            var compiledDirectoryPath = Path.GetDirectoryName(codeBasePath);
+            var jsonFile = Path.Combine(string.Join('\\', compiledDirectoryPath.Split('\\')[..^3]), @"Support\person.json");
+            
+            using (var fileStream = new StreamReader(jsonFile))
+            {
+                byte[] fileBuffer = new byte[fileStream.BaseStream.Length];
+                fileStream.BaseStream.Read(fileBuffer, 0, fileBuffer.Length);
+
+                var reader = new Utf8JsonReader(fileBuffer);
+                var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var person = JsonSerializer.Deserialize<Person>(ref reader, jsonOptions);
+
+                Assert.Equal("Person Name", person.Name);
+                Assert.Equal(18, person.Age);
+                Assert.Equal("Address Example", person.Address);
+                Assert.Equal("02028-525", person.ZipCode);
+            }
         }
     }
 }
